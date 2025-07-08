24240f1b5c2ebe41c2eca5718437f36fb34d07f8
Ben Yoon <benyoon@Bens-MacBook-Pro.local>
2025-07-07 21:57:58 -0400

    lab1

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -5,8 +5,11 @@ export default function Signin() {
       <h3>Sign in</h3>
       <input placeholder="username" className="wd-username" /> <br />
       <input placeholder="password" type="password" className="wd-password" /> <br />
-      <Link  to="/Kambaz/Account/Profile" id="wd-signin-btn"> Sign in </Link> <br />
-      <Link  to="/Kambaz/Account/Signup"  id="wd-signup-link">Sign up</Link>
+      <Link id="wd-signin-btn" to="/Kambaz/Dashboard">
+        Sign in
+      </Link> <br />
+      <Link to="/Kambaz/Account/Signup" id="wd-signup-link">Sign up</Link>
     </div>
-);}
+  );
+}
 
